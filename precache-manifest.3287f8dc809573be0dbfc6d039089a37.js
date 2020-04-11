f33cd974320564b7a96703b70accb75435626d3c
David Eriksson <david_ux@outlook.com>
2020-04-11 12:14:05 +0200

    Updates

---
+++ precache-manifest.3287f8dc809573be0dbfc6d039089a37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203fb1405d64ecc931d528462ce60549",
+    "url": "/aktuella-control/index.html"
+  },
+  {
+    "revision": "981c0cfa6f4bbdb90d1d",
+    "url": "/aktuella-control/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "60a0adf12583a9030094",
+    "url": "/aktuella-control/static/js/2.debe3e43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aktuella-control/static/js/2.debe3e43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981c0cfa6f4bbdb90d1d",
+    "url": "/aktuella-control/static/js/main.fa912168.chunk.js"
+  },
+  {
+    "revision": "4d0675df721ed8d713c7",
+    "url": "/aktuella-control/static/js/runtime-main.e6c6a74e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aktuella-control/static/media/logo.5d5d9eef.svg"
+  }
+]);
